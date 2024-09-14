ef4ca3d657763120c998b587b4fa73d643f195e2
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-09-14 20:24:14 +0200

    Add Zone component and route to it

--- AFELesson3/exercise-03/src/app/app.routes.ts
+++ AFELesson3/exercise-03/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ZoneComponent } from './zone/zone.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'zone', component: ZoneComponent},
+    {path: '', redirectTo: '/zone', pathMatch: 'full'},
+  ];
---
+++ AFELesson3/exercise-03/src/app/zone/zone.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zone',
+  standalone: true,
+  imports: [],
+  templateUrl: './zone.component.html',
+  styleUrl: './zone.component.css'
+})
+export class ZoneComponent {
+
+}
