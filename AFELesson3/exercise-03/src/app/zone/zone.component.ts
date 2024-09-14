934ab61a61721c8e38d08d0977e3c3243cb54447
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-09-14 20:27:50 +0200

    Add content to the Zone component

--- AFELesson3/exercise-03/src/app/zone/zone.component.ts
+++ AFELesson3/exercise-03/src/app/zone/zone.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-zone',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './zone.component.html',
   styleUrl: './zone.component.css'
 })
 export class ZoneComponent {
+  likeAngular = true;
+  count = 0;
+  counter: any;
 
+  toggle() {
+    this.likeAngular = !this.likeAngular;
+  }
+ 
+  startCounter() {
+    this.counter = setInterval(() => {
+      console.log(this.count++);
+    }, 1000);
+  }
+
+  stopCounter() {
+    clearInterval(this.counter);
+  }
 }
