706d9ccd0025ca5d57be151f80893b1299070b27
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-09-15 20:48:11 +0200

    Add Temperature component and add it to navn bar

--- AFELesson3/exercise-03/src/app/app.component.ts
+++ AFELesson3/exercise-03/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { ZoneComponent } from './zone/zone.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ZoneComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- AFELesson3/exercise-03/src/app/app.routes.ts
+++ AFELesson3/exercise-03/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ZoneComponent } from './zone/zone.component';
+import { TemperatureComponent } from './temperature/temperature.component';
 
 export const routes: Routes = [
     {path: 'zone', component: ZoneComponent},
+    {path: 'temperature', component: TemperatureComponent},
     {path: '', redirectTo: '/zone', pathMatch: 'full'},
   ];
