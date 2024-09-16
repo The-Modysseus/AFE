709f5ac714cb1ea5afaa5112d78febb5e3dbef9d
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-09-16 13:32:33 +0200

    Lesson04 exercise 3: Set up routes

--- AFELesson4/lesson04-network-communication/src/app/client/client-routing.module.ts
+++ AFELesson4/lesson04-network-communication/src/app/client/client-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientOverviewComponent } from './client-overview/client-overview.component';
+import { ClientDetailsComponent } from './client-details/client-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "client-overview", component: ClientOverviewComponent},
+  {path: "client-details", component: ClientDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
