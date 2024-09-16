b7b4f1d72a49a15252b218b0803e2ffcef7bdf7f
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-09-16 12:32:42 +0200

    Lesson04 exercise 1: Create application scaffold

---
+++ AFELesson4/lesson04-network-communication/src/app/client/client-overview/client-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-overview.component.html',
+  styleUrl: './client-overview.component.scss'
+})
+export class ClientOverviewComponent {
+
+}
