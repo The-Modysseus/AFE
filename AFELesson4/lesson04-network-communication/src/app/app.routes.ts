68dbe74f4e0c46efefb9a6f9252834ac28f826bf
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-09-16 13:34:51 +0200

    Lesson 4: Fix routing

--- AFELesson4/lesson04-network-communication/src/app/app.routes.ts
+++ AFELesson4/lesson04-network-communication/src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
-import { ClientOverviewComponent } from './client/client-overview/client-overview.component';
-import { ClientDetailsComponent } from './client/client-details/client-details.component';
 
 export const routes: Routes = [
     {path: 'client', loadChildren: () => import('./client/client.module').then(m => m.ClientModule)},
-    {path: "client-overview", component: ClientOverviewComponent},
-    {path: "client-details", component: ClientDetailsComponent}
 ];
