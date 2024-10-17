10e5cfde44d5c096951be2c0fe5d2c9d793426f3
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-10-17 20:40:27 +0200

    Lesson5: Create Form component

---
+++ AFELesson5/form-exercise/src/app/form/form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+
+}
