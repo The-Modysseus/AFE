32071f9fe81e266cdfb520f70d372a041ca4bf7a
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2025-01-05 18:44:39 +0100

    Lesson8: Implement update funktionality

--- AFELesson8/first-pwa-app/src/app/app.component.ts
+++ AFELesson8/first-pwa-app/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  isOnline: boolean;
 
-  constructor() {
+  isOnline: boolean;
+  modalVersion: boolean;
+
+  constructor(private swUpdate: SwUpdate) {
     this.isOnline = false;
+    this.modalVersion = false;
   }
 
   public ngOnInit(): void {
@@ -21,11 +26,30 @@ export class AppComponent implements OnInit {
 
     window.addEventListener('online',  this.updateOnlineStatus.bind(this));
     window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.pipe(
+        filter((evt: any): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+        map((evt: any) => {
+          console.info(`currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]`);
+          this.modalVersion = true;
+        }),
+      ).subscribe();
+    }
   }
 
   private updateOnlineStatus(): void {
     this.isOnline = window.navigator.onLine;
     console.info(`isOnline=[${this.isOnline}]`);
+  }
+
+  public updateVersion(): void {
+    this.modalVersion = false;
+    window.location.reload();
+  }
+
+  public closeVersion(): void {
+    this.modalVersion = false;
   }
 
 }
