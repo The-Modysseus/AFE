3dd66c1d0d352d33b3983d6c22fe72e8d00b467d
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2025-01-05 17:23:46 +0100

    Lesson8: Implement funktion to check network status

--- AFELesson8/first-pwa-app/src/app/app.component.ts
+++ AFELesson8/first-pwa-app/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'first-pwa-app';
+export class AppComponent implements OnInit {
+  isOnline: boolean;
+
+  constructor() {
+    this.isOnline = false;
+  }
+
+  public ngOnInit(): void {
+    this.updateOnlineStatus();
+
+    window.addEventListener('online',  this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+  }
+
+  private updateOnlineStatus(): void {
+    this.isOnline = window.navigator.onLine;
+    console.info(`isOnline=[${this.isOnline}]`);
+  }
+
 }
